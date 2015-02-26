6c965b1c7394f64a21a83262fd0a71078d09c2db
Jake Rorrer <jdrorrer@gmail.com>
2015-02-26 15:20:30 -0500

    Updated collision detection

--- pong.js
+++ pong.js
@@ -22,7 +22,7 @@ var ctx = canvas.getContext('2d');
 // Instantiate game objects
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(300, 200);
+var ball = new Ball(300, 191);
 
 var winningScore = 21;
 
@@ -159,7 +159,7 @@ Computer.prototype.render = function() {
 };
 
 Computer.prototype.update = function(ball) {
-  var y_pos = ball.y;
+  var y_pos = ball.y + 7; // + 7 for radius of ball
   var diff = -((this.paddle.y + (this.paddle.height / 2)) - y_pos); // difference between middle of ball and middle of computer's paddle
   if(diff < 0 && diff < -4) { // max speed up
     diff = -5;
@@ -225,16 +225,16 @@ Ball.prototype.update = function(paddle1, paddle2, score1, score2) {
   this.y += this.y_speed;
 
   // left, right, top and bottom of ball
-  var left_x = this.x;
-  var top_y = this.y;
+  var left_x = this.x + 5;
+  var top_y = this.y + 5;
   var right_x = this.x + 15;
   var bottom_y = this.y + 15;
 
   // Check to see if ball is hitting top or bottom wall
   if(this.y < 0) { // hitting the top wall
-    this.y = 5;
+    this.y = 0;
     this.y_speed = -this.y_speed; // move ball in opposite direction vertically
-  } else if(this.y + 15 > 400) { // hitting the bottom wall
+  } else if(this.y + 20 > 400) { // hitting the bottom wall
     this.y = 380;
     this.y_speed = -this.y_speed;
   }
@@ -250,18 +250,18 @@ Ball.prototype.update = function(paddle1, paddle2, score1, score2) {
     this.x_speed = 3;
     this.y_speed = 0;
     this.x = 300;
-    this.y = 200;
+    this.y = 191;
   }
 
   if(right_x > 300) {
-    if((top_y < (paddle1.y + paddle1.height)) && (bottom_y > paddle1.y) && (left_x <= paddle1.x) && (right_x > paddle1.x)) {
+    if((top_y < (paddle1.y + paddle1.height)) && (bottom_y > paddle1.y) && (left_x <= (paddle1.x + 15)) && (right_x > paddle1.x)) {
       // hit the player's paddle
       this.x_speed = -3;
       this.y_speed += (paddle1.y_speed / 2); // ball moves faster or slower depending on direction of the ball and paddle
       this.x += this.x_speed;
     }
   } else {
-    if((top_y < (paddle2.y + paddle2.height)) && (bottom_y > paddle2.y) && (left_x < (paddle2.x + paddle2.width)) && (right_x >= (paddle2.x + paddle2.width))) {
+    if((top_y < (paddle2.y + paddle2.height)) && (bottom_y > paddle2.y) && (left_x < (paddle2.x + paddle2.width)) && (right_x >= (paddle2.x + paddle2.width - 15))) {
       // hit the computer's paddle
       this.x_speed = 3;
       this.y_speed += (paddle2.y_speed / 2);
